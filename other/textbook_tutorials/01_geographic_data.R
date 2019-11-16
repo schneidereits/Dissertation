ba582f820e07052833ce783baf80eb487e77aa11
schneidereits <s1637673@ed.ac.uk>
2019-11-16 15:37:10 +0000

    bit of intro

--- other/textbook_tutorials/01_geographic_data.R
+++ other/textbook_tutorials/01_geographic_data.R
@@ -6,9 +6,39 @@
 #install.packages("sf")
 #install.packages("raster")
 #install.packages("spData")
-#devtools::install_github("Nowosad/spDataLarge")
-
+# devtools::install_github("Nowosad/spDataLarge")
+#install.packages("devtools")
 # library ----
 library(sf)
 library(raster)
 library(spData)
+library(spDataLarge)   # load larger geographic data
+
+# An introduction to simple features ----
+vignette(package = "sf") # see which vignettes are available
+vignette("sf1")          # an introduction to the package
+
+names(world)
+plot(world)
+
+summary(world["lifeExp"]) #The commonly used summary() function, 
+                          #for example, provides a useful overview of the
+                          #variables within the world object.
+
+world_mini = world[1:2, 1:3]
+world_mini
+library(sp)
+world_sp = as(world, Class = "Spatial")
+world_sf = st_as_sf(world_sp, "sf")
+
+# Basic map making ----
+
+plot(world[3:6])
+plot(world["pop"])
+
+world_asia = world[world$continent == "Asia", ]
+asia = st_union(world_asia)
+plot(world["pop"], reset = FALSE)
+plot(asia, add = TRUE, col = "red")
+
+# 2.2.4 Base plot arguments ----
