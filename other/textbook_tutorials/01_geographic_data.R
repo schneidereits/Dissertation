7a06b8658c69cf3d36beec698753638b9d7d528f
schneidereits <s1637673@ed.ac.uk>
2019-11-16 15:24:32 +0000

    folder for tutorials

---
+++ other/textbook_tutorials/01_geographic_data.R
@@ -0,0 +1,14 @@
+# 16.11.2019
+# Geographic data in R
+
+
+# install ----
+#install.packages("sf")
+#install.packages("raster")
+#install.packages("spData")
+#devtools::install_github("Nowosad/spDataLarge")
+
+# library ----
+library(sf)
+library(raster)
+library(spData)
