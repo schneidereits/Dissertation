ead7de2b5d04fe5fb77213c38c0ef83afe47e0e0
schneidereits <s1637673@ed.ac.uk>
2019-11-17 16:00:35 +0000

    did two sections

--- other/textbook_tutorials/01_geographic_data.R
+++ other/textbook_tutorials/01_geographic_data.R
@@ -39,6 +39,76 @@ plot(world["pop"])
 world_asia = world[world$continent == "Asia", ]
 asia = st_union(world_asia)
 plot(world["pop"], reset = FALSE)
-plot(asia, add = TRUE, col = "red")
+plot(asia, add = TRUE, col = "yellow")
 
 # 2.2.4 Base plot arguments ----
+
+plot(world["continent"], reset = FALSE)
+cex = sqrt(world$pop) / 10000
+world_cents = st_centroid(world, of_largest = TRUE)
+plot(st_geometry(world_cents), add = TRUE, cex = cex)
+
+india = world[world$name_long == "India", ]
+plot(st_geometry(india), expandBB = c(0, 0.2, 0.1, 1), col = "gray", lwd = 3)
+plot(world_asia[0], add = TRUE)
+# dont know why this isnt full size. repeat twice for correct zoom. dont 
+# maybe add function needs to be repeated for proper effect. also has small map
+# on the bottom
+
+# 2.2.5 Geometry types ----
+vignette("sf1")
+POINT (5 2)
+LINESTRING (1 5, 4 4, 4 1, 2 2, 3 2)
+
+GEOMETRYCOLLECTION (MULTIPOINT (5 2, 1 3, 3 4, 3 2), LINESTRING (1 5, 4 4, 4 1, 2 2, 3 2))
+#dont know why these arent working
+
+# 2.2.6 Simple feature geometries (sfg) ----
+
+st_point(c(5, 2))                 # XY point
+#> POINT (5 2)
+st_point(c(5, 2, 3))              # XYZ point
+#> POINT Z (5 2 3)
+st_point(c(5, 2, 1), dim = "XYM") # XYM point
+#> POINT M (5 2 1)
+st_point(c(5, 2, 3, 1))           # XYZM point
+#> POINT ZM (5 2 3 1)
+
+# the rbind function simplifies the creation of matrices
+## MULTIPOINT
+multipoint_matrix = rbind(c(5, 2), c(1, 3), c(3, 4), c(3, 2))
+st_multipoint(multipoint_matrix)
+#> MULTIPOINT (5 2, 1 3, 3 4, 3 2)
+## LINESTRING
+linestring_matrix = rbind(c(1, 5), c(4, 4), c(4, 1), c(2, 2), c(3, 2))
+st_linestring(linestring_matrix)
+#> LINESTRING (1 5, 4 4, 4 1, 2 2, 3 2)
+
+## POLYGON
+polygon_list = list(rbind(c(1, 5), c(2, 2), c(4, 1), c(4, 4), c(1, 5)))
+st_polygon(polygon_list)
+#> POLYGON ((1 5, 2 2, 4 1, 4 4, 1 5))
+## POLYGON with a hole
+polygon_border = rbind(c(1, 5), c(2, 2), c(4, 1), c(4, 4), c(1, 5))
+polygon_hole = rbind(c(2, 4), c(3, 4), c(3, 3), c(2, 3), c(2, 4))
+polygon_with_hole_list = list(polygon_border, polygon_hole)
+st_polygon(polygon_with_hole_list)
+#> POLYGON ((1 5, 2 2, 4 1, 4 4, 1 5), (2 4, 3 4, 3 3, 2 3, 2 4))
+## MULTILINESTRING
+multilinestring_list = list(rbind(c(1, 5), c(4, 4), c(4, 1), c(2, 2), c(3, 2)), 
+                            rbind(c(1, 2), c(2, 4)))
+st_multilinestring((multilinestring_list))
+#> MULTILINESTRING ((1 5, 4 4, 4 1, 2 2, 3 2), (1 2, 2 4))
+## MULTIPOLYGON
+multipolygon_list = list(list(rbind(c(1, 5), c(2, 2), c(4, 1), c(4, 4), c(1, 5))),
+                         list(rbind(c(0, 2), c(1, 2), c(1, 3), c(0, 3), c(0, 2))))
+st_multipolygon(multipolygon_list)
+#> MULTIPOLYGON (((1 5, 2 2, 4 1, 4 4, 1 5)), ((0 2, 1 2, 1 3, 0 3, 0 2)))
+## GEOMETRYCOLLECTION
+gemetrycollection_list = list(st_multipoint(multipoint_matrix),
+                              st_linestring(linestring_matrix))
+st_geometrycollection(gemetrycollection_list)
+#> GEOMETRYCOLLECTION (MULTIPOINT (5 2, 1 3, 3 4, 3 2),
+#>   LINESTRING (1 5, 4 4, 4 1, 2 2, 3 2))
+
+# none of this is working...
