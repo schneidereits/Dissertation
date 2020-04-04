77c331f261737df87e927d20e9a4b9dfe3dac40b
schneidereits <s1637673@ed.ac.uk>
2020-04-04 14:11:02 +0100

    flipping KO plots background cover and biodiv

--- code/QHI_biodiversity.R
+++ code/QHI_biodiversity.R
@@ -81,5 +81,102 @@ pointfr_2018_2019 <- QHI_pointframe_full %>%
                           SUBSITE == "KO" & PLOT == 6 ~ 1))
   
 
-mutate(cg = case_when(carb <= 2 ~ "low",
-                      carb > 2  ~ "high")
+cover_2018_2019 <- QHI_cover_1999_2019_sas %>%
+  filter(year == "2018" | year == "2019") %>%
+  # flip KO plots are they were found to be reverse. HE stays the same (not pretty but it works)
+  mutate(PLOT = case_when(sub_name == "QHI:HE" & PLOT == 1 ~ 1,
+                          sub_name == "QHI:HE" & PLOT == 2 ~ 2,
+                          sub_name == "QHI:HE" & PLOT == 3 ~ 3,
+                          sub_name == "QHI:HE" & PLOT == 4 ~ 4,
+                          sub_name == "QHI:HE" & PLOT == 5 ~ 5,
+                          sub_name == "QHI:HE" & PLOT == 6 ~ 6,
+                          sub_name == "QHI:KO" & PLOT == 1 ~ 6,
+                          sub_name == "QHI:KO" & PLOT == 2 ~ 5,
+                          sub_name == "QHI:KO" & PLOT == 3 ~ 4,
+                          sub_name == "QHI:KO" & PLOT == 4 ~ 3,
+                          sub_name == "QHI:KO" & PLOT == 5 ~ 2,
+                          sub_name == "QHI:KO" & PLOT == 6 ~ 1))
+
+
+# ** Bareground cover data ----
+
+# source code: Qikiqtaruk Ecological Monitoring manuscript script
+# Code for all modelling and data visualisation within the manuscript
+# Written by Isla Myers-Smith, Anne Bjorkman, Haydn Thomas, Sandra Angers-Blondin and Gergana Daskalova
+
+pointfr_2018_2019$uniqueID <- paste(pointfr_2018_2019$YEAR, pointfr_2018_2019$SUBSITE, 
+                                    pointfr_2018_2019$PLOT, pointfr_2018_2019$X, pointfr_2018_2019$Y, sep="")  # Assign unique ID to every point
+bareground_IDs <- pointfr_2018_2019[pointfr_2018_2019$SPP=="XXXlitter" | pointfr_2018_2019$SPP=="XXXlitter " | 
+                              pointfr_2018_2019$SPP=="XXXbareground" | pointfr_2018_2019$SPP=="XXXbareground " | 
+                              pointfr_2018_2019$SPP=="XXXrock" | pointfr_2018_2019$SPP =="XXXrock " | 
+                              pointfr_2018_2019$SPP =="XXXfeces" | pointfr_2018_2019$SPP =="XXXfeces " | 
+                              pointfr_2018_2019$SPP =="XXXstandingwater" | pointfr_2018_2019$SPP =="XXXstandingwater " | 
+                              pointfr_2018_2019$SPP =="XXXspider", "uniqueID"]  # Identify points with non-vegetation indicators
+pointfr_2018_2019_BGs <- pointfr_2018_2019[pointfr_2018_2019$uniqueID %in% bareground_IDs,]  # Extract only points that have non-veg indicators
+
+Out=NULL  # Set up loop
+for(i in unique(pointfr_2018_2019_BGs$uniqueID)){  # For each point
+  a <- subset(pointfr_2018_2019_BGs, uniqueID==i)  # create dataframe of all entries for that point
+  b <- a[a$SPP == "XXXlitter" | a$SPP == "XXXlitter " | a$SPP == "XXXbareground" | 
+           a$SPP == "XXXbareground " | a$SPP == "XXXrock" | a$SPP == "XXXrock " | 
+           a$SPP == "XXXfeces" | a$SPP == "XXXfeces " | a$SPP == "XXXstandingwater" | 
+           a$SPP == "XXXstandingwater " | a$SPP == "XXXspider",]  # Identify how many entries are not vegetation
+  c <- nrow(a) - nrow(b)  # Finnd out if any entries are vegetation (i.e. total rows - non-veg rows)
+  Out <- rbind(Out, c(i, c))  # Extract point name and number vegetation entries
+}
+
+BGs <- as.data.frame(Out)  # Convert into data frame
+BGs <- subset(BGs,V2=="0")  # Extract points for which there are only non-veg data (i.e.i.e. total rows - non-veg rows = 0)
+BGs <- BGs[,1]  # Extract only first column (unique points)
+
+bareground <- pointfr_2018_2019_BGs <- pointfr_2018_2019[pointfr_2018_2019$uniqueID %in% BGs,]  # Create dataframe of bare ground points 
+bareground <- ddply(bareground,.(YEAR, PLOT, SUBSITE), summarise,
+                    Bareground = sum(Abundance))  # Count number of bare ground points per plot
+
+# Create dummy dataframe with all plots because if plots have no bare ground they wont be included
+bareground_full <- ddply(pointfr_2018_2019,.(YEAR, SUBSITE, PLOT), summarise,
+                         Bareground = 0)  
+
+# Replace dummy bareground with real baregound
+bareground_full$Bareground <- bareground$Bareground[match(paste(bareground_full$YEAR,
+                                                                bareground_full$SUBSITE,
+                                                                bareground_full$PLOT),
+                                                          paste(bareground$YEAR,
+                                                                bareground$SUBSITE,
+                                                                bareground$PLOT))] 
+bareground <- bareground_full  # Rename to original
+bareground[is.na(bareground$Bareground),]$Bareground <- 0  # Replace NAs from match with zeros
+
+
+# ** Community composition and diversity measures ----
+
+# richness 
+#from GD spectra hub
+
+richness <- cover_2018_2019 %>% 
+  # removing non species 
+  filter(!Species %in% c("XXXbareground", "XXXfeces",
+                         "XXXlitter", "XXXstandingwater")) %>%
+  select(sub_name, PLOT, Species, year) %>%
+  distinct() %>%
+  group_by(sub_name, PLOT, year) %>% tally()
+
+colnames(richness)[3] <- "richness"
+
+# Remove rows with no cover
+diversity <- subset(cover_2018_2019, cover>0)
+# Remove non veg
+diversity <- subset(diversity, Species !="XXXlitter" & Species !="XXXlitter " & 
+                      Species !="XXXbareground" & Species !="XXXbareground " & 
+                      Species !="XXXrock" & Species !="XXXrock " & 
+                      Species !="XXXfeces" & Species !="XXXfeces " & 
+                      Species !="XXXstandingwater" & Species !="XXXstandingwater " & 
+                      Species !="XXXspider"& Species !="Xxxspider"& Species !="XXXspider ")
+# Add unique plots
+diversity$plot_unique <- paste(diversity$sub_name,diversity$plot,diversity$year,sep="")
+
+# Convert to relative cover
+plot_cover <- ddply(diversity,.(plot_unique), summarise,
+                    total_cover = sum(cover))
+diversity$total_cover <- plot_cover$total_cover[match(diversity$plot_unique, plot_cover$plot_unique)]
+diversity$rel_cover <- diversity$cover/diversity$total_cover*100
