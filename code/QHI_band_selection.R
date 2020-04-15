2eadde3566e73c8685eaea22db400529dbe1f8df
schneidereits <s1637673@ed.ac.uk>
2020-04-15 07:31:16 +0100

    made supervised selection 2018+2019

--- code/QHI_band_selection.R
+++ code/QHI_band_selection.R
@@ -3,7 +3,6 @@
 # 5.3.2020
 
 
-
 #  band selection ----
 
 # supervised band selection
@@ -18,8 +17,8 @@ supervised_band_selection <- tibble(wavelength =
                                         seq(920, 985, by = 0.01)))# Vascular plant structures & H20 
 
 supervised_band_selection <- QHI_2018_2019 %>%
-  filter(!type == "mixed", 
-         wavelength %in% supervised_band_selection$wavelength) %>%
+  #filter(!type == "mixed") %>% 
+  filter(wavelength %in% supervised_band_selection$wavelength) %>%
   group_by(type, plot, id, year) %>%
   summarise(spec_mean = mean(reflectance),
             CV = mean(sd(reflectance)/mean(reflectance)))
@@ -198,6 +197,10 @@ QHI_ISI_tbl <- QHI_ISI_band_selection %>%
   summarise(selected_ISI = sum(ISI)) %>%
   left_join(QHI_ISI_tbl) 
 
+QHI_ISI_tbl <- QHI_ISI_tbl[c("region", "ISI", "relative_ISI",  "wavebands_selected", "selected_ISI")]
+
+
+
 
 QHI_SZU <- QHI_ISI %>%
   arrange(ISI) %>%
@@ -244,190 +247,13 @@ ggplot(QHI_ISI, aes(x=wavelength, y=ISI)) +
 
 # plot of trends in accumulated D_ISIi values
 ggplot(QHI_SZU, aes(x=n, y=D_ISIi)) +
+  #geom_point(QHI_SZU, aes(x=3, y = D_ISIi, color="red")) +
   # hardcode to match total number of selected wavebands USE: sum(QHI_ISI_tbl$wavebands_selected)
   geom_vline(xintercept = 24, linetype="dotted") + # need to pick correct vline
+  geom_vline(xintercept = 3, linetype="dashed", color="red") + # need to pick correct vline
   geom_line() +
   labs(x= "Number of bands selected ") +
   theme_cowplot()
 
 #ggsave(p_SZU, path = "figures", filename = "SZU.png", height = 10, width = 12)
 
-# H3 models (band selection) ----
-
-# linear model with supervised band selection 2018+2019
-
-m_H3a <- lmer(data = supervised_band_selection, spec_mean ~ type + year + (1|plot))
-
-summary(m_H3a)
-
-plot(m_H3a)
-qqnorm(resid(m_H3a))
-qqline(resid(m_H3a)) 
-
-
-# Visualises random effects 
-(re.effects <- plot_model(m_H3a, type = "re", show.values = TRUE))
-# visulise fixed effect
-(fe.effects <- plot_model(m_H3a, show.values = TRUE))
-
-
-# CV
-
-(hist <- ggplot(supervised_band_selection, aes(x = CV)) +
-    geom_histogram() +
-    theme_classic())
-
-# linear model with band selection
-
-m_H3b <- lmer(data = supervised_band_selection, CV ~ type + year + (1|plot))
-
-summary(m_H3b)
-
-plot(m_H3b)
-qqnorm(resid(m_H3b))
-qqline(resid(m_H3b)) 
-
-
-# Visualises random effects 
-(re.effects <- plot_model(m_H3b, type = "re", show.values = TRUE))
-# visulise fixed effect
-(fe.effects <- plot_model(m_H3b, show.values = TRUE))
-
-
-# linear model with supervised band selection only 2019
-
-supervised_band_selection_2019 <- supervised_band_selection %>%
-  filter(year == 2019)
-
-m_H3c <- lmer(data = supervised_band_selection_2019, spec_mean ~ type + (1|plot))
-
-summary(m_H3c)
-
-plot(m_H3c)
-qqnorm(resid(m_H3c))
-qqline(resid(m_H3c)) 
-
-
-# Visualises random effects 
-(re.effects <- plot_model(m_H3c, type = "re", show.values = TRUE))
-# visulise fixed effect
-(fe.effects <- plot_model(m_H3c, show.values = TRUE))
-
-
-# CV
-
-(hist <- ggplot(supervised_band_selection_2019, aes(x = CV)) +
-    geom_histogram() +
-    theme_classic())
-
-
-# models with supervised band selection for dimention reduction 
-
-m_H3d <- lmer(data = supervised_band_selection_2019, CV ~ type + (1|plot))
-
-summary(m_H3d)
-
-plot(m_H3d)
-qqnorm(resid(m_H3d))
-qqline(resid(m_H3d)) 
-
-
-# Visualises random effects 
-(re.effects <- plot_model(m_H3d, type = "re", show.values = TRUE))
-# visulise fixed effect
-(fe.effects <- plot_model(m_H3d, show.values = TRUE))
-
-
-# ISI band selecrtion models
-
-(hist <- ggplot(lowD, aes(x = spec_mean)) +
-    geom_histogram() +
-    theme_classic())
-
-# linear model with ISI band selection (2019 only)
-
-m_H3e <- lmer(data = QHI_lowD, spec_mean ~ type + year + (1|plot))
-
-summary(m_H3e)
-
-plot(m_H3e)
-qqnorm(resid(m_H3e))
-qqline(resid(m_H3e)) 
-
-
-# Visualises random effects 
-(re.effects <- plot_model(m_H3e, type = "re", show.values = TRUE))
-# visulise fixed effect
-(fe.effects <- plot_model(m_H3e, show.values = TRUE))
-
-
-# CV
-
-(hist <- ggplot(lowD, aes(x = CV)) +
-    geom_histogram() +
-    theme_classic())
-
-# linear model with band selection
-
-m_H3f <- lmer(data = lowD, CV ~ type + (1|plot))
-
-summary(m_H3f)
-
-plot(m_H3f)
-qqnorm(resid(m_H3f))
-qqline(resid(m_H3f)) 
-
-
-# Visualises random effects 
-(re.effects <- plot_model(m_H3f, type = "re", show.values = TRUE))
-# visulise fixed effect
-(fe.effects <- plot_model(m_H3f, show.values = TRUE))
-
-
-# combined model vis
-
-# spectral mean
-(p_H3a <- dwplot(list(m_H1a, m_H3a, m_H3c, m_H3e), 
-                 vline = geom_vline(xintercept = 0, colour = "grey60", linetype = 1)) +
-    theme_cowplot() +
-    theme(legend.position = c(0.8, 0.2),
-          legend.justification = c(0, 0),
-          legend.title.align = .5))
-
-ggsave(p_H3a, path = "figures", filename = "H3_models_mean.png", height = 10, width = 12)
-
-# CV
-# effect sizes and error dont seem to correspond with model summary...
-(p_H3b <-dwplot(list(m_H1b, m_H3b, m_H3d, m_H3f), 
-                vline = geom_vline(xintercept = 0, colour = "grey60", linetype = 1)) +
-    theme_cowplot() +
-    theme(legend.position = c(0.8, 0.2),
-          legend.justification = c(0, 0),
-          legend.title.align = .5))
-
-ggsave(p_H3b, path = "figures", filename = "H3_models_cv.png", height = 10, width = 12)
-
-grid.arrange(p_H3a, p_H3b)  
-
-
-# If needed I could use ggpredict to creat boxplot of predicted spec_mean and cv by VT by Model (but might not be compatable with lme4)
-# https://strengejacke.github.io/ggeffects/reference/ggpredict.html
-
-ggpredict(m_H3a, terms = c("type"), type = "fe") %>% 
-  plot(rawdata = TRUE) +
-  #scale_color_manual(values = c("#ffa544", "#2b299b")) +
-  theme_cowplot()
-
-ggpredict(m_H3b, terms = c("type", "year"), type = "fe") %>% 
-  plot(rawdata = TRUE) +
-  #scale_color_manual(values = c("#ffa544", "#2b299b")) +
-  theme_cowplot()
-
-dat <- ggpredict(H3a, terms = c("c172code", "c161sex"))
-ggplot(H3a, aes(type, predicted, colour = group)) +
-  geom_point(position = position_dodge(.1)) +
-  geom_errorbar(
-    aes(ymin = conf.low, ymax = conf.high),
-    position = position_dodge(.1)
-  ) +
-  scale_x_discrete(breaks = 1:3, labels = get_x_labels(dat))
