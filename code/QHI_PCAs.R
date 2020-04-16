ec0e339e040f3cb0b166bfe592e6d15d26a0e290
schneidereits <s1637673@ed.ac.uk>
2020-04-16 17:07:40 +0100

    updated PCA

--- code/QHI_PCAs.R
+++ code/QHI_PCAs.R
@@ -6,10 +6,10 @@
 #  QHI PCA ----
 
 # for all QHI measurements(2019)
-pca <- QHI_small 
+pca <- spec_2019_small 
 
 # detailed pca; adapted from: http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/112-pca-principal-component-analysis-essentials/#pca-data-format
-res.pca <- PCA(pca[,c(5,7)], scale.unit = TRUE, ncp = 5, graph = TRUE)
+res.pca <- PCA(pca[,c(4,5)], scale.unit = TRUE, ncp = 5, graph = TRUE)
 
 # pca results
 print(res.pca)
@@ -21,11 +21,11 @@ eig.val
 
 
 # grouped by elipsise
-(p_pca_QHI <- fviz_pca_biplot(res.pca,
+(p_pca_QHI <- fviz_pca_ind(res.pca,
                               geom.ind = "point", # show points only (nbut not "text")
                               col.ind = "black", # color by groups
                               pointshape = 21,
-                              fill.ind = QHI_small$type, # color by groups
+                              fill.ind = spec_2019_small$type, # color by groups
                               palette = c("#ffa544", "#2b299b", "gray65"),
                               addEllipses = TRUE, # Concentration ellipses
                               # ellipse.type = "confidence",
@@ -41,21 +41,32 @@ ggsave(p_pca_QHI, path = "figures", filename = "QHI_pca.png", height = 10, width
 
 
 # pca for all QHI measurements, with band selection (2019)
-pca_lowD <- QHI %>%
+pca_lowD <- spec_2019 %>%
   filter(wavelength %in% ISI_band_selection$wavelength) %>%
-  group_by(type, plot, id) %>%
+  group_by(year, type, plot, plot_unique, type_year, wavelength) %>%
   summarise(spec_mean = mean(reflectance),
-            CV = mean(sd(reflectance)/mean(reflectance)))
+            spec_SD = sd(reflectance),
+            CV = sd(reflectance)/mean(reflectance)) %>%
+  # MIGHT WHAT TO IMPROVE BUT I KNOW THIS WORKS...
+  group_by(type, plot_unique, year) %>%
+  summarise(CV = mean(CV),
+            spec_mean = mean(spec_mean))
+
 
 # both with new band selection?
-pca_lowD <- QHI_2018_2019 %>%
+pca_lowD <- spec_2018_2019 %>%
   mutate(wavelength = round(wavelength, digits = 0)) %>%
   filter(wavelength %in% QHI_ISI_band_selection$wavelength) %>%
-  group_by(type, plot, id) %>%
+  group_by(year, type, plot, plot_unique, type_year, wavelength) %>%
   summarise(spec_mean = mean(reflectance),
-            CV = mean(sd(reflectance)/mean(reflectance)))
+            spec_SD = sd(reflectance),
+            CV = sd(reflectance)/mean(reflectance)) %>%
+  # MIGHT WHAT TO IMPROVE BUT I KNOW THIS WORKS...
+  group_by(type, plot_unique, year, type_year) %>%
+  summarise(CV = mean(CV),
+            spec_mean = mean(spec_mean))
 
-res.pca_lowD <- PCA(pca_lowD[,4:5], scale.unit = TRUE, ncp = 5, graph = TRUE)
+res.pca_lowD <- PCA(pca_lowD[,5:6], scale.unit = TRUE, ncp = 5, graph = TRUE)
 
 # pca results
 print(res.pca_lowD)
@@ -103,7 +114,7 @@ fviz_pca_var(res.pca, col.var = "contrib",
 (p_pca <- fviz_pca_ind(res.pca_lowD,
                           geom.ind = "point", # show points only (nbut not "text")
                           pointshape = 21,
-                         fill.ind = QHI_2018_2019_small$type_year, # color by groups
+                         fill.ind = spec_2018_2019_small$type_year, # color by groups
                           col.ind = "black", # color by groups
                          # palette = c("#ffa544", "#2b299b", "gray65"),
                           palette = c( "tomato", "#ffa544", "purple", "#2b299b", "gray65"),
@@ -119,12 +130,12 @@ fviz_pca_var(res.pca, col.var = "contrib",
 ggsave(p_pca, path = "figures", filename = "QHI_lowD_pca.png", height = 10, width = 12)
 
 # biplot
-(p_pca <- fviz_pca_biplot(res.pca_lowD,
+(p_pca <- fviz_pca_ind(res.pca_lowD,
                           repel = TRUE, 
                           geom.ind = "point", # show points only (nbut not "text")
                           pointshape = 21,
-                          fill.ind = QHI_small$type, # color by groups
-                          col.ind = QHI_small$type, # color by groups
+                          fill.ind = spec_2018_2019_small$type, # color by groups
+                          col.ind = spec_2018_2019_small$type, # color by groups
                           palette = c("#ffa544", "#2b299b", "gray65"),
                           addEllipses = TRUE, # Concentration ellipses
                           # ellipse.type = "confidence",
@@ -137,7 +148,7 @@ ggsave(p_pca, path = "figures", filename = "QHI_lowD_pca.png", height = 10, widt
 
 
 # multi year PCA
-res.pca_QHI_2018_2019 <- PCA(QHI_2018_2019_small[,c(5,7)], scale.unit = TRUE, ncp = 5, graph = TRUE)
+res.pca_QHI_2018_2019 <- PCA(spec_2018_2019_small[,c(5,6)], scale.unit = TRUE, ncp = 5, graph = TRUE)
 
 # pca 
 (p_pca <- fviz_pca_ind(res.pca_QHI_2018_2019,
@@ -145,7 +156,7 @@ res.pca_QHI_2018_2019 <- PCA(QHI_2018_2019_small[,c(5,7)], scale.unit = TRUE, nc
                        pointshape = 21,
                        col.ind = "black",
                        fill.ind = QHI_2018_2019_small$type, # color by groups
-                       #palette = c("#ffa544", "#2b299b", "gray65"),
+                       palette = c("#ffa544", "#2b299b", "gray65"),
                        addEllipses = TRUE, # Concentration ellipses
                        # ellipse.type = "confidence",
                        ellipse.level = 0.95, # confidence level specification
@@ -161,8 +172,8 @@ ggsave(p_pca, path = "figures", filename = "QHI_lowD_biplot.png", height = 10, w
 
 (p_pca_veg_year <- fviz_pca_ind(res.pca_QHI_2018_2019,
                                 geom.ind = "point", # show points only (nbut not "text")
-                                fill.ind = QHI_2018_2019_small$type_year, # color by groups
-                                color.ind = QHI_2018_2019_small$type_year,
+                                fill.ind = spec_2018_2019_small$type_year, # color by groups
+                                color.ind = spec_2018_2019_small$type_year,
                                 pointshape = 21,
                                 palette = c( "tomato", "#ffa544", "purple", "#2b299b", "gray65"),
                                 addEllipses = TRUE, # Concentration ellipses
@@ -262,7 +273,7 @@ pca_H2_2018_2019 <- collison_spec_plot_small
 head(pca_H2)
 
 # ncp = 10 (10 variables)
-res.pca_H2_2018_2019 <- PCA(pca_H2_2018_2019[,c(6, 9:16)], scale.unit = TRUE, ncp = 9, graph = TRUE)
+res.pca_H2_2018_2019 <- PCA(pca_H2_2018_2019[,c(5, 8:15)], scale.unit = TRUE, ncp = 9, graph = TRUE)
 
 # eigen values
 
@@ -359,7 +370,7 @@ fviz_pca_var(res.pca_H2_2018_2019, col.var = "contrib",
 
 # pca by type and year
 # 
-collison_spec_plot_small <- collison_spec_plot_small %>%
+t <- collison_spec_plot_small %>%
   # add site variable 
   unite(site, c(type,year))
 
