c2a4c44ddbefb22acd7b125052263b09a18677f7
schneidereits <s1637673@ed.ac.uk>
2020-04-17 07:58:49 +0100

    models changed to lm

--- code/QHI_models.R
+++ code/QHI_models.R
@@ -8,25 +8,27 @@
 
 # model 2018 + 2019 HE & KO and mixed
 
+spec_2018_2019_small$year <- as.factor(spec_2018_2019_small$year)
+
 # histogram 
-(hist <- ggplot(QHI_2018_2019_small, aes(x = spec_mean)) +
+(hist <- ggplot(spec_2018_2019_small, aes(x = spec_mean)) +
    geom_histogram() +
    theme_classic())
 
 # does not converge
-summary(lmer(data = QHI_2018_2019_small, spec_mean ~ type + year + (1|plot))) # does not converge
+summary(lm(data = spec_2018_2019_small, spec_mean ~ type + year)) # does not converge
 
 # model only 2018+2019 only HE and KO
 
-(hist <- ggplot(collison_2018_2019_small, aes(x = spec_mean)) +
+(hist <- ggplot(spec_2018_2019_small, aes(x = spec_mean)) +
     geom_histogram() +
     theme_classic())
 
 # linear model for H1
 
-m_H1a <- (lmer(data = collison_2018_2019_small, spec_mean ~ type + year + (1|plot))) # (type-1) changes intercpt to HE 
+m_H1a <- (lm(data = spec_2018_2019_small, spec_mean ~ (type-1) +year)) # (type-1) changes intercpt to HE 
 
-# temporary additon of mixed
+          # temporary additon of mixed
 m_H1a <- (lmer(data = QHI_2018_2019_small, spec_mean ~ type + year + (1|plot))) # (type-1) changes intercpt to HE 
 
 
@@ -50,6 +52,7 @@ qqline(resid(m_H1a))  # points fall nicely onto the line - good!
 pred.mm <- ggpredict(m_H1a, terms = c("type")) %>%  # this gives overall predictions for the model
   rename(type = x) %>%
   mutate(type = as.character(type))
+
 
 H1a_prediction <- collison_wavelength %>%
   group_by(type, wavelength) %>%
@@ -110,11 +113,11 @@ veg.cover <- ggplot() +
 # CV model
 
 # QHI 2018 + 2019 model
-(hist <- ggplot(QHI_2018_2019_small, aes(x = CV)) +
+(hist <- ggplot(spec_2018_2019_small, aes(x = CV)) +
     geom_histogram() +
     theme_classic())
 
-lmer(data = QHI_2018_2019_small, CV ~ (type-1) + year + (1|plot))
+lmer(data = spec_2018_2019_small, CV ~ (type-1) + year + (1|plot))
 # does converge but for consistence should leave out
 
 
@@ -123,13 +126,16 @@ lmer(data = QHI_2018_2019_small, CV ~ (type-1) + year + (1|plot))
     geom_histogram() +
     theme_classic())
 
-m_H1b <- lmer(data = collison_2018_2019_small, CV ~ type + year + (1|plot))
+m_H1b <- lm(data = spec_2018_2019_small, CV ~ type + year)
 
 # temporary addition of mixed
-m_H1b <- lmer(data = QHI_2018_2019_small, CV ~ type + year + (1|plot))
+m_H1b <- lmer(data = QHI_2018_2019_small, CV ~ type- + year + (1|plot))
 
 
 summary(m_H1b)
+
+stargazer(m_H1b, type = "text")
+
 
 plot(m_H1b)
 qqnorm(resid(m_H1b))
@@ -150,8 +156,10 @@ ggpredict(m_H1b, terms = c("type"), type = "fe") %>%
 # H3 models (band selection) ----
 
 # linear model with supervised band selection 2018+2019
+supervised_band_selection$year <-  as.factor(supervised_band_selection$year)
 
-m_H3a <- lmer(data = supervised_band_selection, spec_mean ~ type + year + (1|plot))
+
+m_H3a <- lm(data = supervised_band_selection, spec_mean ~ type + year)
 
 summary(m_H3a)
 
@@ -174,7 +182,7 @@ qqline(resid(m_H3a))
 
 # linear model with band selection
 
-m_H3b <- lmer(data = supervised_band_selection, CV ~ type + year + (1|plot))
+m_H3b <- lm(data = supervised_band_selection, CV ~ type + year)
 
 summary(m_H3b)
 
@@ -189,6 +197,152 @@ qqline(resid(m_H3b))
 (fe.effects <- plot_model(m_H3b, show.values = TRUE))
 
 
+
+
+############ ISI band selecrtion models
+
+(hist <- ggplot(lowD, aes(x = spec_mean)) +
+    geom_histogram() +
+    theme_classic())
+
+# linear model with ISI band selection 
+
+QHI_lowD$year <- as.factor(QHI_lowD$year)
+
+m_H3e <- lm(data = QHI_lowD, spec_mean ~ type + year)
+
+summary(m_H3e)
+
+plot(m_H3e)
+qqnorm(resid(m_H3e))
+qqline(resid(m_H3e)) 
+
+
+# Visualises random effects 
+(re.effects <- plot_model(m_H3e, type = "re", show.values = TRUE))
+# visulise fixed effect
+(fe.effects <- plot_model(m_H3e, show.values = TRUE))
+
+
+# CV
+
+(hist <- ggplot(QHI_lowD, aes(x = CV)) +
+    geom_histogram() +
+    theme_classic())
+
+# linear model with band selection
+
+m_H3f <- lm(data = QHI_lowD, CV ~ type + year)
+
+summary(m_H3f)
+
+plot(m_H3f)
+qqnorm(resid(m_H3f))
+qqline(resid(m_H3f)) 
+
+
+# Visualises random effects 
+(re.effects <- plot_model(m_H3f, type = "re", show.values = TRUE))
+# visulise fixed effect
+(fe.effects <- plot_model(m_H3f, show.values = TRUE))
+
+
+# combined model vis
+
+# spectral mean
+(p_H3a <- dwplot(list(m_H1a, m_H3a, m_H3e), 
+                 vline = geom_vline(xintercept = 0, colour = "grey60", linetype = 1)) +
+    theme_cowplot() +
+    coord_flip() +
+    theme(legend.position = c(0.8, 0.2),
+          legend.justification = c(0, 0),
+          legend.title.align = .5))
+
+ggsave(p_H3a, path = "figures", filename = "H3_models_mean.png", height = 10, width = 12)
+
+# CV
+# effect sizes and error dont seem to correspond with model summary...
+(p_H3b <-dwplot(list(m_H1b, m_H3b, m_H3f), 
+                vline = geom_vline(xintercept = 0, colour = "grey60", linetype = 1)) +
+    theme_cowplot() +
+    coord_flip() +
+    theme(legend.position = c(0.8, 0.2),
+          legend.justification = c(0, 0),
+          legend.title.align = .5))
+
+ggsave(p_H3b, path = "figures", filename = "H3_models_cv.png", height = 10, width = 12)
+
+grid.arrange(p_H3a, p_H3b)  
+
+
+# If needed I could use ggpredict to creat boxplot of predicted spec_mean and cv by VT by Model (but might not be compatable with lme4)
+# https://strengejacke.github.io/ggeffects/reference/ggpredict.html
+
+ggpredict(m_H3a, terms = c("type"), type = "fe") %>% 
+  plot(rawdata = TRUE) +
+  #scale_color_manual(values = c("#ffa544", "#2b299b")) +
+  theme_cowplot()
+
+ggpredict(m_H3b, terms = c("type", "year"), type = "fe") %>% 
+  plot(rawdata = TRUE) +
+  #scale_color_manual(values = c("#ffa544", "#2b299b")) +
+  theme_cowplot()
+
+dat <- ggpredict(H3a, terms = c("c172code", "c161sex"))
+ggplot(H3a, aes(type, predicted, colour = group)) +
+  geom_point(position = position_dodge(.1)) +
+  geom_errorbar(
+    aes(ymin = conf.low, ymax = conf.high),
+    position = position_dodge(.1)
+  ) +
+  scale_x_discrete(breaks = 1:3, labels = get_x_labels(dat))
+
+
+
+
+(p_H1a <- ggpredict(m_H1a, terms = c("type"), type = "fe") %>% 
+    plot(rawdata = TRUE) +
+    scale_color_manual(values = c("#ffa544", "#2b299b", "grey")) +
+    theme_cowplot())
+
+
+(p_H3a <- ggpredict(m_H3a, terms = c("type"), type = "fe") %>% 
+    plot(rawdata = TRUE) +
+    #scale_color_manual(values = c("#ffa544", "#2b299b")) +
+    ylim(0,0.6) +
+    theme_cowplot())
+
+(p_H3e <- ggpredict(m_H3e, terms = c("type"), type = "fe") %>% 
+    plot(rawdata = TRUE) +
+    #scale_color_manual(values = c("#ffa544", "#2b299b")) +
+    theme_cowplot())
+
+# CV predicitons
+(p_H1b <- ggpredict(m_H1b, terms = c("type"), type = "fe") %>% 
+    plot(rawdata = FALSE) +
+    scale_color_manual(values = c("#ffa544", "#2b299b", "grey")) +
+    ylim(-0.05,0.4) +
+    theme_cowplot())
+
+
+(p_H3b <- ggpredict(m_H3b, terms = c("type", "year"), type = "fe") %>% 
+    plot(rawdata = TRUE) +
+    #scale_color_manual(values = c("#ffa544", "#2b299b")) ++
+    ylim(-0.05,0.4) +
+    theme_cowplot())
+
+(p_H3f <- ggpredict(m_H3f, terms = c("type"), type = "fe") %>% 
+    plot(rawdata = TRUE) +
+    #scale_color_manual(values = c("#ffa544", "#2b299b")) +
+    ylim(-0.05,0.4) +
+    theme_cowplot())
+
+grid.arrange(p_H1a, p_H3a, p_H3e, 
+             p_H1b, p_H3b, p_H3f, nrow=2) 
+
+
+#############.
+#############.
 # linear model with supervised band selection only 2019
 
 supervised_band_selection_2019 <- supervised_band_selection %>%
@@ -233,58 +387,54 @@ qqline(resid(m_H3d))
 (fe.effects <- plot_model(m_H3d, show.values = TRUE))
 
 
-############ ISI band selecrtion models
+#  model with ISI band selection only 2019
 
-(hist <- ggplot(lowD, aes(x = spec_mean)) +
-    geom_histogram() +
-    theme_classic())
 
-# linear model with ISI band selection (2019 only)
+m_H3g <- lm(data = lowD_2019selection, spec_mean ~ type + year)
 
-m_H3e <- lmer(data = QHI_lowD, spec_mean ~ type + year + (1|plot))
+summary(m_H3g)
 
-summary(m_H3e)
-
-plot(m_H3e)
-qqnorm(resid(m_H3e))
-qqline(resid(m_H3e)) 
+plot(m_H3g)
+qqnorm(resid(m_H3g))
+qqline(resid(m_H3g)) 
 
 
 # Visualises random effects 
-(re.effects <- plot_model(m_H3e, type = "re", show.values = TRUE))
+(re.effects <- plot_model(m_H3g, type = "re", show.values = TRUE))
 # visulise fixed effect
-(fe.effects <- plot_model(m_H3e, show.values = TRUE))
+(fe.effects <- plot_model(m_H3g, show.values = TRUE))
 
 
 # CV
 
-(hist <- ggplot(QHI_lowD, aes(x = CV)) +
+(hist <- ggplot(supervised_band_selection_2019, aes(x = CV)) +
     geom_histogram() +
     theme_classic())
 
-# linear model with band selection
 
-m_H3f <- lmer(data = QHI_lowD, CV ~ type + year + (1|plot))
+# models with supervised band selection for dimention reduction 
 
-summary(m_H3f)
+m_H3h <- lm(data = lowD_2019selection, CV ~ type +year)
 
-plot(m_H3f)
-qqnorm(resid(m_H3f))
-qqline(resid(m_H3f)) 
+summary(m_H3h)
+
+plot(m_H3h)
+qqnorm(resid(m_H3h))
+qqline(resid(m_H3h)) 
 
 
 # Visualises random effects 
-(re.effects <- plot_model(m_H3f, type = "re", show.values = TRUE))
+(re.effects <- plot_model(m_H3h, type = "re", show.values = TRUE))
 # visulise fixed effect
-(fe.effects <- plot_model(m_H3f, show.values = TRUE))
-
+(fe.effects <- plot_model(m_H3h, show.values = TRUE))
 
 # combined model vis
 
 # spectral mean
-(p_H3a <- dwplot(list(m_H1a, m_H3a, m_H3e), 
+(p_H3a <- dwplot(list(m_H1a, m_H3a, m_H3e, m_H3g), 
                  vline = geom_vline(xintercept = 0, colour = "grey60", linetype = 1)) +
     theme_cowplot() +
+    coord_flip() +
     theme(legend.position = c(0.8, 0.2),
           legend.justification = c(0, 0),
           legend.title.align = .5))
@@ -293,9 +443,10 @@ ggsave(p_H3a, path = "figures", filename = "H3_models_mean.png", height = 10, wi
 
 # CV
 # effect sizes and error dont seem to correspond with model summary...
-(p_H3b <-dwplot(list(m_H1b, m_H3b, m_H3f), 
+(p_H3b <-dwplot(list(m_H1b, m_H3b, m_H3f, m_H3h), 
                 vline = geom_vline(xintercept = 0, colour = "grey60", linetype = 1)) +
     theme_cowplot() +
+    coord_flip() +
     theme(legend.position = c(0.8, 0.2),
           legend.justification = c(0, 0),
           legend.title.align = .5))
@@ -305,73 +456,18 @@ ggsave(p_H3b, path = "figures", filename = "H3_models_cv.png", height = 10, widt
 grid.arrange(p_H3a, p_H3b)  
 
 
-# If needed I could use ggpredict to creat boxplot of predicted spec_mean and cv by VT by Model (but might not be compatable with lme4)
-# https://strengejacke.github.io/ggeffects/reference/ggpredict.html
-
-ggpredict(m_H3a, terms = c("type"), type = "fe") %>% 
-  plot(rawdata = TRUE) +
-  #scale_color_manual(values = c("#ffa544", "#2b299b")) +
-  theme_cowplot()
-
-ggpredict(m_H3b, terms = c("type", "year"), type = "fe") %>% 
-  plot(rawdata = TRUE) +
-  #scale_color_manual(values = c("#ffa544", "#2b299b")) +
-  theme_cowplot()
-
-dat <- ggpredict(H3a, terms = c("c172code", "c161sex"))
-ggplot(H3a, aes(type, predicted, colour = group)) +
-  geom_point(position = position_dodge(.1)) +
-  geom_errorbar(
-    aes(ymin = conf.low, ymax = conf.high),
-    position = position_dodge(.1)
-  ) +
-  scale_x_discrete(breaks = 1:3, labels = get_x_labels(dat))
 
 
+##############.
+##############.
 
 
-(p_H1a <- ggpredict(m_H1a, terms = c("type"), type = "fe") %>% 
-    plot(rawdata = TRUE) +
-    scale_color_manual(values = c("#ffa544", "#2b299b", "grey")) +
-    theme_cowplot())
-
-
-(p_H3a <- ggpredict(m_H3a, terms = c("type", "year"), type = "fe") %>% 
-    plot(rawdata = TRUE) +
-    #scale_color_manual(values = c("#ffa544", "#2b299b")) +
-    theme_cowplot())
-
-(p_H3e <- ggpredict(m_H3e, terms = c("type"), type = "fe") %>% 
-    plot(rawdata = TRUE) +
-    #scale_color_manual(values = c("#ffa544", "#2b299b")) +
-    theme_cowplot())
-
-# CV predicitons
-(p_H1b <- ggpredict(m_H1b, terms = c("type"), type = "fe") %>% 
-    plot(rawdata = FALSE) +
-    scale_color_manual(values = c("#ffa544", "#2b299b", "grey")) +
-    ylim(.2,1) +
-    theme_cowplot())
-
-
-(p_H3b <- ggpredict(m_H3b, terms = c("type", "year"), type = "fe") %>% 
-    plot(rawdata = TRUE) +
-    #scale_color_manual(values = c("#ffa544", "#2b299b")) +
-    theme_cowplot())
-
-(p_H3f <- ggpredict(m_H3f, terms = c("type"), type = "fe") %>% 
-    plot(rawdata = TRUE) +
-    #scale_color_manual(values = c("#ffa544", "#2b299b")) +
-    theme_cowplot())
-
-grid.arrange(p_H1a, p_H3a, p_H3e, 
-             p_H1b, p_H3b, p_H3f, nrow=2) 
 
 # H2 model ----
 
 # correlation plot full
 
-correlation <- cor(collison_spec_plot_small[,c(6, 9:16)])
+correlation <- cor(collison_spec_plot_small[,c(5, 8:15)])
 
 print(collison_spec_plot_small)
 
@@ -381,7 +477,7 @@ print(collison_spec_plot_small)
 
 # correlation plot for model
 
-correlation_small <- cor(collison_spec_plot_small[,c(6, 9:10, 15:16)])
+correlation_small <- cor(collison_spec_plot_small[,c(5, 8:9, 14:15)])
 
 (p_corr <- corrplot(correlation_small, method="circle", type="upper", #order="hclust",
                     tl.srt=45, tl.col="black", diag = FALSE, col=brewer.pal(n=10, name="RdYlBu")))
@@ -400,23 +496,23 @@ chart.Correlation(correlation, histogram=TRUE, pch=19)
     theme_classic())
 
 
-# linear model for H2
+# linear model for H2 2018 2019
 
 # spectral mean
 
-collison_spec_plot_small_2019 <- collison_spec_plot_small %>% filter(year == 2019) #%>%
+#collison_spec_plot_small_2019 <- collison_spec_plot_small %>% filter(year == 2019) #%>%
 # to normalize evenness
-mutate(evenness = (evenness- min(evenness))/(max(evenness)-min(evenness)))
+#mutate(evenness = (evenness- min(evenness))/(max(evenness)-min(evenness)))
 
 # to scale all continous varibales
-collison_spec_plot_small_2019$richness <- scale(collison_spec_plot_small_2019$richness)
-collison_spec_plot_small_2019$evenness <- scale(collison_spec_plot_small_2019$evenness)
-collison_spec_plot_small_2019$bareground <- scale(collison_spec_plot_small_2019$bareground)
+collison_spec_plot_small$richness <- scale(collison_spec_plot_small$richness)
+collison_spec_plot_small$evenness <- scale(collison_spec_plot_small$evenness)
+collison_spec_plot_small$bareground <- scale(collison_spec_plot_small$bareground)
 
-str(collison_spec_plot_small_2019)
+str(collison_spec_plot_small)
 
-m_H2a <- lmer(data = collison_spec_plot_small_2019,
-              spec_mean ~ (type-1) * (type*richness) + (type*evenness) + (type*bareground) + (1|plot))
+m_H2a <- lm(data = collison_spec_plot_small,
+              spec_mean ~ (type-1) * (type*richness) + (type*evenness) + (type*bareground))
 
 
 summary(m_H2a)
@@ -424,6 +520,16 @@ summary(m_H2a)
 plot(m_H2a)
 qqnorm(resid(m_H2a))
 qqline(resid(m_H2a)) 
+
+
+p_H2a_base <- allEffects(m_H2a)
+print(p_H2a_base)
+
+plot(p_H2a_base)
+
+
+
+
 
 # Visualises random effects 
 (re.effects <- plot_model(m_H2a, type = "re", show.values = TRUE))
@@ -433,17 +539,6 @@ qqline(resid(m_H2a))
 
 # attempting to visulize model output
 ggpredict(data = m_H2a, c("type", "richness")) %>% plot()
-
-
-
-e <- allEffects(m_H2a)
-print(e)
-
-plot(e)
-
-e.df <- as.data.frame(e)
-
-ggplot(fe.effects)
 
 
 ggplot(e.df$`type:richness`, aes(x=richness, y=fit, color=type, ymin=lower, ymax=upper)) + 
@@ -495,20 +590,32 @@ grid.arrange(p_H2a_rich, p_H2a_even, p_H2a_ground, nrow = 1)
 
 
 
-# CV
+# CV 2018 2019
 
-(hist <- ggplot(collison_spec_plot_small_2019, aes(x = CV)) +
+(hist <- ggplot(collison_spec_plot_small, aes(x = CV)) +
     geom_histogram() +
     theme_classic())
 
-m_H2b <- lmer(data = collison_spec_plot_small_2019, 
-              CV ~ (type-1) + (type*richness) + (type*evenness) + (type*bareground) + (1|plot))
+m_H2b <- lm(data = collison_spec_plot_small, 
+              CV ~ (type-1) + (type*richness) + (type*evenness) + (type*bareground))
 
 summary(m_H2b)
 
 plot(m_H2b)
 qqnorm(resid(m_H2b))
 qqline(resid(m_H2b)) 
+
+# spectral mean (richness, eveness, and bareground)
+p_H2b_base <- allEffects(m_H2b)
+
+print(p_H2b_base)
+plot(p_H2b_base)
+
+# specmean and cv outputs together
+
+arrange(p_H2a_base, p_H2b_base)
+
+
 
 # Visualises random effects 
 (re.effects <- plot_model(m_H2b, type = "re", show.values = TRUE))
