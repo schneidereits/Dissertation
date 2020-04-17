c40ae4ebed7d4f55b9a9f163257c2e234d3ffe4c
schneidereits <s1637673@ed.ac.uk>
2020-04-17 07:58:10 +0100

    rearranged order

--- code/QHI_band_selection.R
+++ code/QHI_band_selection.R
@@ -89,116 +89,6 @@ supervised_band_selection <- spec_2018_2019 %>%
    coord_cartesian(ylim =c(3,50)) +
    theme_rgb_mean
 
- 
- 
- 
-# ISI band selection and SZU 0NLY 2019
-
-collison_ISI <- collison %>%
-  filter(type %in% c("KO" , "HE")) %>%
-  group_by(wavelength) %>%
-  summarise(ISI = ((mean(reflectance[type=="HE"]) + mean(reflectance[type=="KO"]))*1.96)/
-              (abs(sd(reflectance[type=="HE"] - sd(reflectance[type=="KO"]))))) %>%
-  mutate(region = case_when(between(wavelength, 400, 500) ~ "blue",		
-                            between(wavelength, 500, 600) ~ "green",		
-                            between(wavelength, 600, 680) ~ "red",		
-                            between(wavelength, 680, 800) ~ "NIR",		
-                            between(wavelength, 800, 1000) ~ "IR")) 
-
-
-ISI_band_selection <- collison_ISI %>%
-  mutate(n = row_number()) %>%
-  # filter wavelengths that are local ISI minima; (-) is to denote minima
-  filter(n %in% find_peaks(-collison_ISI$ISI, window = 3)) %>%
-  mutate(region = case_when(between(wavelength, 400, 500) ~ "blue",		
-                            between(wavelength, 500, 600) ~ "green",		
-                            between(wavelength, 600, 680) ~ "red",		
-                            between(wavelength, 680, 800) ~ "NIR",		
-                            between(wavelength, 800, 1000) ~ "IR")) 
-
-# ISI by region
-ISI_tbl <- collison_ISI %>%
-  group_by(region) %>%
-  summarise(ISI = sum(ISI))
-
-# number relative ISI (as groups include differnt numbers of wavebands)
-ISI_tbl <- collison_ISI %>%
-  group_by(region) %>%
-  count(region) %>%
-  left_join(ISI_tbl) %>%
-  # relative ISI colunm (ISI* portotional size of region)
-  mutate(relative_ISI = ISI/(n/123)) # need to change value with number of bands in largest region
-
-# number wavebands selected 
-ISI_tbl <- ISI_band_selection %>%
-  group_by(region) %>%
-  count(region) %>%
-  rename(wavebands_selected = n) %>%
-  right_join(ISI_tbl, value="region")
-
-# ISI of these selected wavebands   
-ISI_tbl <- ISI_band_selection %>%
-  group_by(region) %>%
-  summarise(selected_ISI = sum(ISI)) %>%
-  left_join(ISI_tbl) 
-
-
-
-SZU <- collison_ISI %>%
-  arrange(ISI) %>%
-  mutate(ISI = as.numeric(ISI),
-         n = row_number(),
-         d_ISI = (lead(ISI)/ISI - 1) *100,
-         # 0.015 is the trade-off value (q)
-         d_qi = (0.015- d_ISI),
-         # number of bands is equal to max DISIi 
-         D_ISIi = cumsum(d_qi))
-
-# the selcted wavelengths according to SZU
-head(SZU, n=5)
-
-# reduced dimentionality; product of ISI band selection
-lowD <- spec_2018_2019 %>%
-  filter(wavelength %in% ISI_band_selection$wavelength) %>%
-  group_by(type, plot, id, year) %>%
-  summarise(spec_mean = mean(reflectance),
-            CV = mean(sd(reflectance)/mean(reflectance)))
-
-# need to spruce up https://www.rdocumentation.org/packages/ggpmisc/versions/0.3.3/topics/stat_peaks
-# plot of ISI by wavelength and local minima
-(p_ISI <-  ggplot(collison_ISI, aes(x=wavelength, y=ISI)) +
-    geom_line() +
-    theme_cowplot() +
-    geom_point(data = ISI_band_selection) +
-    #stat_valleys(span = 3, shape = 1, size = 2, color = "black", fill = NA) +
-    scale_y_continuous(expand = expand_scale(mult = c(0, .1))) +
-    scale_x_continuous(expand = expand_scale(mult = c(0, .1))) +
-    annotate("rect", xmin = 400, xmax = 500, ymin = 16,
-             ymax = 23, alpha = .15, fill = "blue") + 
-    annotate("rect", xmin = 500, xmax = 600, ymin = 16, 
-             ymax = 23, alpha = .15, fill = "green") +
-    annotate("rect", xmin = 600, xmax = 680, ymin = 16, 
-             ymax = 23, alpha = .15, fill = "red") + 
-    annotate("rect", xmin = 680, xmax = 800, ymin = 16, 
-             ymax = 23, alpha = .15, fill = "tomato") +
-    annotate("rect", xmin = 800, xmax = 985, ymin = 16, 
-             ymax = 23, alpha = .15, fill = "darkgrey"))
-
-#ggsave(p_ISI, path = "figures", filename = "ISI_by_wavelength.png", height = 10, width = 12)
-
-# plot of trends in accumulated D_ISIi values
-(p_SZU <- ggplot(SZU, aes(x=n, y=D_ISIi)) +
-    # hardcode to match total number of selected wavebands USE: sum(ISI_tbl$wavebands_selected)
-    geom_vline(xintercept = 25, linetype="dotted") + 
-    geom_line() +
-    labs(x= "Number of bands selected ") +
-    theme_cowplot())
-
-
-
-#ggsave(p_SZU, path = "figures", filename = "SZU.png", height = 10, width = 12)
-
-
 ############# 2018 2019 ISI band selection and SZU 
 
 # inelegant solution to selecting only 2018 wavebands that have a matching 2019 evivalent 
@@ -269,7 +159,7 @@ QHI_ISI_tbl <- QHI_ISI_tbl[c("region", "ISI", "relative_ISI",  "wavebands_select
 
 
 QHI_SZU <- QHI_ISI %>%
-  arrange(ISI) %>%
+  arrange(ISI) %>%qhi
   mutate(ISI = as.numeric(ISI),
          n = row_number(),
          d_ISI = (lead(ISI)/ISI - 1) *100,
@@ -334,6 +224,121 @@ ggplot(QHI_SZU, aes(x=n, y=D_ISIi)) +
   geom_line() +
   labs(x= "Number of bands selected ") +
   theme_cowplot()
+
+#ggsave(p_SZU, path = "figures", filename = "SZU.png", height = 10, width = 12)
+
+
+
+# ISI band selection and SZU 0NLY 2019
+
+collison_ISI <- spec_2018_2019 %>%
+  filter(type %in% c("KO" , "HE"),
+         year == 2019) %>%
+  group_by(wavelength) %>%
+  summarise(ISI = ((mean(reflectance[type=="HE"]) + mean(reflectance[type=="KO"]))*1.96)/
+              (abs(sd(reflectance[type=="HE"] - sd(reflectance[type=="KO"]))))) %>%
+  mutate(region = case_when(between(wavelength, 400, 500) ~ "blue",		
+                            between(wavelength, 500, 600) ~ "green",		
+                            between(wavelength, 600, 680) ~ "red",		
+                            between(wavelength, 680, 800) ~ "NIR",		
+                            between(wavelength, 800, 1000) ~ "IR")) 
+
+
+ISI_band_selection <- collison_ISI %>%
+  mutate(n = row_number()) %>%
+  # filter wavelengths that are local ISI minima; (-) is to denote minima
+  filter(n %in% find_peaks(-collison_ISI$ISI, window = 3)) %>%
+  mutate(region = case_when(between(wavelength, 400, 500) ~ "blue",		
+                            between(wavelength, 500, 600) ~ "green",		
+                            between(wavelength, 600, 680) ~ "red",		
+                            between(wavelength, 680, 800) ~ "NIR",		
+                            between(wavelength, 800, 1000) ~ "IR")) 
+
+# ISI by region
+ISI_tbl <- collison_ISI %>%
+  group_by(region) %>%
+  summarise(ISI = sum(ISI))
+
+# number relative ISI (as groups include differnt numbers of wavebands)
+ISI_tbl <- collison_ISI %>%
+  group_by(region) %>%
+  count(region) %>%
+  left_join(ISI_tbl) %>%
+  # relative ISI colunm (ISI* portotional size of region)
+  mutate(relative_ISI = ISI/(n/123)) # need to change value with number of bands in largest region
+
+# number wavebands selected 
+ISI_tbl <- ISI_band_selection %>%
+  group_by(region) %>%
+  count(region) %>%
+  rename(wavebands_selected = n) %>%
+  right_join(ISI_tbl, value="region")
+
+# ISI of these selected wavebands   
+ISI_tbl <- ISI_band_selection %>%
+  group_by(region) %>%
+  summarise(selected_ISI = sum(ISI)) %>%
+  left_join(ISI_tbl) 
+
+
+
+SZU <- collison_ISI %>%
+  arrange(ISI) %>%
+  mutate(ISI = as.numeric(ISI),
+         n = row_number(),
+         d_ISI = (lead(ISI)/ISI - 1) *100,
+         # 0.015 is the trade-off value (q)
+         d_qi = (0.015- d_ISI),
+         # number of bands is equal to max DISIi 
+         D_ISIi = cumsum(d_qi))
+
+# the selcted wavelengths according to SZU
+head(SZU, n=5)
+
+# reduced dimentionality; product of ISI band selection
+lowD_2019selection <- spec_2018_2019 %>%
+ # round so that 2018 and 2019 wavelengths match
+  mutate(round(wavelength, digits = 0)) %>%
+  group_by(year, type, plot, plot_unique, type_year, wavelength) %>%
+  summarise(spec_mean = mean(reflectance),
+            spec_SD = sd(reflectance),
+            CV = sd(reflectance)/mean(reflectance)) %>%
+  # MIGHT WHAT TO IMPROVE BUT I KNOW THIS WORKS...
+  group_by(type, plot_unique, year) %>%
+  summarise(CV = mean(CV),
+            spec_mean = mean(spec_mean))
+
+# need to spruce up https://www.rdocumentation.org/packages/ggpmisc/versions/0.3.3/topics/stat_peaks
+# plot of ISI by wavelength and local minima
+(p_ISI <-  ggplot(collison_ISI, aes(x=wavelength, y=ISI)) +
+    geom_line() +
+    theme_cowplot() +
+    geom_point(data = ISI_band_selection) +
+    #stat_valleys(span = 3, shape = 1, size = 2, color = "black", fill = NA) +
+    scale_y_continuous(expand = expand_scale(mult = c(0, .1))) +
+    scale_x_continuous(expand = expand_scale(mult = c(0, .1))) +
+    annotate("rect", xmin = 400, xmax = 500, ymin = 16,
+             ymax = 23, alpha = .15, fill = "blue") + 
+    annotate("rect", xmin = 500, xmax = 600, ymin = 16, 
+             ymax = 23, alpha = .15, fill = "green") +
+    annotate("rect", xmin = 600, xmax = 680, ymin = 16, 
+             ymax = 23, alpha = .15, fill = "red") + 
+    annotate("rect", xmin = 680, xmax = 800, ymin = 16, 
+             ymax = 23, alpha = .15, fill = "tomato") +
+    annotate("rect", xmin = 800, xmax = 985, ymin = 16, 
+             ymax = 23, alpha = .15, fill = "darkgrey"))
+
+#ggsave(p_ISI, path = "figures", filename = "ISI_by_wavelength.png", height = 10, width = 12)
+
+# plot of trends in accumulated D_ISIi values
+(p_SZU <- ggplot(SZU, aes(x=n, y=D_ISIi)) +
+    # hardcode to match total number of selected wavebands USE: sum(ISI_tbl$wavebands_selected)
+    geom_vline(xintercept = 25, linetype="dotted") + 
+    geom_line() +
+    labs(x= "Number of bands selected ") +
+    theme_cowplot())
+
+
 
 #ggsave(p_SZU, path = "figures", filename = "SZU.png", height = 10, width = 12)
 
