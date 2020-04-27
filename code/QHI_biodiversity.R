c1cc277f3fa10634fb5da8e730e8db98cc373ef6
schneidereits <s1637673@ed.ac.uk>
2020-04-27 14:37:59 +0100

    wd

--- code/QHI_biodiversity.R
+++ code/QHI_biodiversity.R
@@ -6,12 +6,12 @@ library(vegan)
 
 # loading data ---- 
 setwd("~/Documents/university work/Dissertation/Dissertation")
-QHI_pointframe <- read.csv("Dissertation/data/QHI_biodiversity/Herschel_ITEXdata_1999-2018_updated.csv")
+QHI_pointframe <- read.csv("data/QHI_biodiversity/Herschel_ITEXdata_1999-2018_updated.csv")
 head(QHI_pointframe)
 levels(QHI_pointframe$SPP) # Checking all spelling is correct
 unique(QHI_pointframe$YEAR) # Check years
 
-QHI_cover <- read.csv("Dissertation/data/QHI_biodiversity/QHI_cover_1999_2019_sas.csv")
+QHI_cover <- read.csv("data/QHI_biodiversity/QHI_cover_1999_2019_sas.csv")
 head(QHI_cover)
 levels(QHI_cover$name) # Checking all spelling is correct
 unique(QHI_cover$year) # Check years
